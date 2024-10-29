26f4d57722ca06e26591490b7391cf48fdcaa790
Peter Rigole <peter.rigole@atomicbits.io>
2024-10-29 12:15:34 +0100

    Now successfully added tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ async function greet() {
 
 <template>
   <main class="container">
-    <h1>Welcome to Tauri + Vue</h1>
+    <h1 class="text-3xl font-bold" >Welcome to Tauri + Vue</h1>
 
     <div class="row">
       <a href="https://vitejs.dev" target="_blank">
